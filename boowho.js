5a1fd3c55d1c2fc32926a45a2f236528675f4e61
iankiema <kiemaian.ik@gmail.com>
2024-01-22 12:09:44 +0300

    solved the boowho problem

---
+++ boowho.js
@@ -0,0 +1,10 @@
+function boowho(bool){
+    if (bool === true || bool === false) {
+        return true 
+    } else {
+        return false
+    }
+}
+
+console.log(boowho(true))
+console.log(boowho([1,2,3]))
