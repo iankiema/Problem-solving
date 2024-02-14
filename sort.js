0ce7078f9ca48184d25963c4be95c16b5b0e1216
iankiema <kiemaian.ik@gmail.com>
2024-02-14 18:55:45 +0300

    Updated the sort problem

--- sort.js
+++ sort.js
@@ -1,7 +1,15 @@
-function ascendingOrder(arr) {
+// function ascendingOrder(arr) {
+//     return arr.sort(function(a,b) {
+//         return a - b
+//     })
+// }
+
+// console.log(ascendingOrder([1,5,2,3,4]))
+
+function alphabeticalOrder(arr) {
     return arr.sort(function(a,b) {
-        return a - b
+        return a === b ? 0 : b < a ? 1 : -1
     })
 }
 
-console.log(ascendingOrder([1,5,2,3,4]))
+console.log(alphabeticalOrder(["a","d","c","a","z","g"]))
